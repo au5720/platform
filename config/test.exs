29299234ad5d65c4d95d32e79bb864eade9b7c7a
Jennifer <jennifermorgan2001@gmail.com>
2020-04-09 16:53:50 +0000

    JSON API setup working

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ config :platform, Platform.Repo,
   database: "platform_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
-
+  
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :platform, PlatformWeb.Endpoint,
