c43a50589cf3203d769ec8389150b175a5289044
Jennifer <jennifermorgan2001@gmail.com>
2020-04-21 14:08:34 +0000

    Working hard coded game list

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,8 +1,114 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
+
+-- MAIN
+
+
 main =
-    text "Hello THAR - Elm inside Phoenix!"
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { gamesList : List Game
+    , displayGameList : Bool
+    }
+
+
+type alias Game =
+    { title : String
+    , description : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { gamesList =
+        [ { title = "Platform Game", description = "Platform game example" }
+        , { title = "AdventureGame", description = "Adventure game example" }
+        ]
+    , displayGameList = True
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = DisplayGameList
+    | HideGameList
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        DisplayGameList ->
+            ( { model | displayGameList = True }, Cmd.none )
+
+        HideGameList ->
+            ( { model | displayGameList = False }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [ class "games-section" ] [ text "Games" ]
+        , button [ class "button", onClick DisplayGameList ] [ text "Display Games List" ]
+        , button [ class "button", onClick HideGameList ] [ text "Hide Games List" ]
+        , if model.displayGameList then
+            gameIndex model
+
+          else
+            div [] []
+        ]
+
+
+gameIndex : Model -> Html msg
+gameIndex model =
+    div [ class "games-index" ] [ gameList model.gamesList ]
+
+
+gameList : List Game -> Html msg
+gameList games =
+    ul [ class "games-list" ] (List.map gameListItem games)
+
+
+gameListItem : Game -> Html msg
+gameListItem game =
+    li []
+        [ strong [] [ text game.title ]
+        , p [] [ text game.description ]
+        ]
